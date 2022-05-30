c2310124c593bafe46d7a84c5efa9bbd11e17dce
Grimstar15 <ttsegtsee@gmail.com>
2022-05-30 12:10:54 +0800

    tseg

--- index.js
+++ index.js
@@ -13,7 +13,38 @@ app.get("/", (req, res) => {
   res.render("start");
 });
 app.get("/lab3", (req, res) => {
-  res.render("lab3");
+  const yaml = require("js-yaml");
+  const fs = require("fs");
+
+  // Get document, or throw exception on error
+  try {
+    const doc = yaml.load(fs.readFileSync("./lab3.grc", "utf8"));
+    console.log(doc.options.parameters.title);
+    res.render("lab3", {
+      data: {
+        title: doc.options.parameters.title,
+        output_language: doc.options.parameters.output_language,
+        generate_options: doc.options.parameters.generate_options,
+        block_freq: doc.blocks[0].name,
+        block_freq_label: doc.blocks[0].parameters.label,
+        block_freq_start: doc.blocks[0].parameters.start,
+        block_freq_end: doc.blocks[0].parameters.end,
+        block_freq_value: doc.blocks[0].parameters.value,
+        block_freq_throt: doc.blocks[0].parameters.throttle,
+        noise_amp: doc.blocks[1].parameters.name,
+        noise_amp_label: doc.blocks[1].parameters.label,
+        noise_amp_start: doc.blocks[1].parameters.start,
+        noise_amp_end: doc.blocks[1].parameters.end,
+        noise_amp_value: doc.blocks[1].parameters.value,
+        noise_amp_throt: doc.blocks[1].parameters.throttle,
+        variable: doc.blocks[2].name,
+        variable_val: doc.blocks[2].parameters.value,
+        noise_source_seed: doc.blocks[4].parameters.serr,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
@@ -24,3 +55,14 @@ process.on("unhandledRejection", (err, promise) => {
     process.exit(1);
   });
 });
+
+const yaml = require("js-yaml");
+const fs = require("fs");
+
+// Get document, or throw exception on error
+try {
+  const doc = yaml.load(fs.readFileSync("./lab3.grc", "utf8"));
+  console.log(doc.blocks[4].parameters);
+} catch (e) {
+  console.log(e);
+}
